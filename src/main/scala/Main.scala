b6e80f3297427853515b5af703a4c39906fd8e1f
August Nagro <augustnagro@gmail.com>
2023-06-19 10:37:04 -0700

    recursive type issue

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 case class User(firstName: String, age: Int)
 
 @main def main: Unit =
+  // This works
+  /*
   val user = Builder[User]
     .withFirstName("Athena")
     .withAge(22)
     .build
   println(user.age)
+  println(user)
+   */
+
+  // But use the wrong method name, and scalac stack overflows.
+  // I believe this is because the RecursiveType usage.
+  val user = Builder[User].wrongName(22).build
   println(user)
